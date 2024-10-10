e65cadfe2948aa8a595e8fcca433802dfb3c983e
CharanKondaveeti <91211319+CharanKondaveeti@users.noreply.github.com>
2024-10-10 10:01:23 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.PNG','**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.svg'], // Include these file types as assets
+  assetsInclude: ['**/*.PNG','**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.svg'],
+  "rewrites": [
+      { "source": "/(.*)", "destination": "/" }
+    ]
 });
